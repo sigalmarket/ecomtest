1da960e2ae6c1878108a92bd159834c9c07fc8c7
sigalmarket <anthony.vella.2409.2@gmail.com>
2019-05-08 13:27:28 +0200

    Updates

---
+++ precache-manifest.f4332d373f825e9efb1ca31d4e75b502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6633fcb0a38464280ce2a3a4fd82b71",
+    "url": "/ecomtest/index.html"
+  },
+  {
+    "revision": "1b130c99cec249d8130e",
+    "url": "/ecomtest/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/ecomtest/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1b130c99cec249d8130e",
+    "url": "/ecomtest/static/js/main.61df3be1.chunk.js"
+  },
+  {
+    "revision": "73fd60f2b007133c7430",
+    "url": "/ecomtest/static/js/runtime~main.83a29af1.js"
+  }
+]);
